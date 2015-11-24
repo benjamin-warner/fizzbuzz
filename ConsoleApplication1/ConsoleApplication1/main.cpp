e0743b77f940eb755af02e6cea2f839569e0af19
Benjamin Warner <benjaminnwarner@gmail.com>
2015-11-24 11:32:46 -0500

    FizzBuzz part 2

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -10,6 +10,8 @@ using std::to_string;
 string Fizz(int);
 string Buzz(int);
 string returnNonFizzBuzzable(int);
+string containsThree(int);
+string containsFive(int);
 void FizzBuzz();
 
 int main(int argc, char* argv[]){
@@ -28,7 +30,21 @@ int main(int argc, char* argv[]){
 	assert(returnNonFizzBuzzable(5) == "");
 	assert(returnNonFizzBuzzable(7) == "7");
 	assert(returnNonFizzBuzzable(10) == "");
+	assert(returnNonFizzBuzzable(35) == "");
+	assert(returnNonFizzBuzzable(51) == "");
+	assert(returnNonFizzBuzzable(31) == "");
 
+	assert(containsThree(3) == "Fizz");
+	assert(containsThree(5) == "");
+	assert(containsThree(13) == "Fizz");
+	assert(containsThree(30) == "Fizz");
+	assert(containsThree(33) == "Fizz");
+
+	assert(containsFive(5) == "Buzz");
+	assert(containsFive(3) == "");
+	assert(containsFive(15) == "Buzz");
+	assert(containsFive(50) == "Buzz");
+	assert(containsFive(55) == "Buzz");
 
 	cout << endl;
 	FizzBuzz();
@@ -51,18 +67,42 @@ string Buzz(int num){
 }
 
 string returnNonFizzBuzzable(int num){
-	if(Fizz(num) == ""
-	&& Buzz(num) == "")
+	if(Fizz(num) == "" && Buzz(num) == ""
+		&& containsFive(num) == "" && containsThree(num) == "")
 		return to_string(num);
+	else
+		return "";
+}
+
+string containsThree(int num){
+	//3 in tens place
+	string temp = to_string(num);
+	if(temp[0] == '3' || temp[1] == '3')
+		return "Fizz";
+	else
+		return "";
+}
+
+string containsFive(int num){
+	string temp = to_string(num);
+	if(temp[0] == '5' || temp[1] == '5')
+		return "Buzz";
 	else
 		return "";
 }
 
 void FizzBuzz(){
 	for(int i = 1; i <= 100; ++i){
-		cout << Fizz(i);
-		cout << Buzz(i);
+		if(Fizz(i) == "Fizz" || containsThree(i) == "Fizz")
+			cout << "Fizz";
+		if(Buzz(i) == "Buzz" || containsFive(i) == "Buzz")
+			cout << "Buzz";
 		cout << returnNonFizzBuzzable(i);
 		cout << endl;
+
+		//cout << Fizz(i);
+		//cout << Buzz(i);
+		//cout << returnNonFizzBuzzable(i);
+		//cout << endl;
 	}
 }
